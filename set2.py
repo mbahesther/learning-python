cdf8c3735c0a8e803f03d2316e4971d5c1256998
mbah esther <esthermabah@gmail.com>
2022-07-04 14:56:06 +0100

    completed set set2.py

--- set2.py
+++ set2.py
@@ -32,4 +32,37 @@ print(thisset)
 # looping the set with for loop
 thisset = {"head", "nose", "eye", "leg"}
 for x in thisset:
-    print(x)
+    #print(x)
+ 
+ #join two set using union
+ set1 = {"mango", "apple", "melon"} 
+set2 = {"bag", "shoe", "weavon"}
+sett = set1.union(set2)
+print(sett)
+
+#update
+set1 = {"mango", "apple", "melon"}
+set2 = {"bag"}
+set1.update(set2)
+print(set1)
+
+#The intersection_update() method will keep only the items that are present in both sets.
+x = {"microsoft", "google", "speedpay"}
+y = {"ice", "microsoft", "speedpay"}
+x.intersection_update(y)
+print(x)
+
+
+ #The symmetric_difference_update() method will keep only the elements 
+ # that are NOT present in both sets.
+a = {"telephone", "spar", "amazon", "apple"}
+b = {"adobe", "apple", "catfish"}
+
+a.symmetric_difference_update(b)
+print(a)
+
+#Return a set that contains all items from both sets, except items that are present in both:
+a = {"sweet", "biscuit", "yoghurt"}
+b = {"grape", "pineaple", "sweet", "yoghurt"}
+c = a.symmetric_difference(b)
+print(c)
